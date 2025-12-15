8008edffa1adf93f3700afeb7a5b7ef9ae0acb02
ldallavecchia <lucadv90@gmail.com>
2025-12-15 11:29:17 +0800

    first commit

---
+++ src/main/java/com/ldv/kafka_order_inventory_poc/KafkaOrderInventoryPocApplication.java
@@ -0,0 +1,13 @@
+package com.ldv.kafka_order_inventory_poc;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class KafkaOrderInventoryPocApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(KafkaOrderInventoryPocApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/ldv/kafka_order_inventory_poc/KafkaOrderInventoryPocApplicationTests.java
@@ -0,0 +1,13 @@
+package com.ldv.kafka_order_inventory_poc;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class KafkaOrderInventoryPocApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
